d7c93688c395cc37485bc2cc0a1c95085af94ca0
rparitosh <rparitosh@gmail.com>
2022-06-17 13:44:35 -0500

    Threading example using Tjread

--- src/main/java/com/paritosh/learning/java/multithreading/MultiThreadExample.java
+++ src/main/java/com/paritosh/learning/java/multithreading/MultiThreadExample.java
@@ -1,4 +1,37 @@
 package com.paritosh.learning.java.multithreading;
 
+
 public class MultiThreadExample {
+    public static void main (String[] args){
+        Hi hi = new Hi();
+        Hello hello = new Hello();
+
+
+        hi.start();
+        try{Thread.sleep(100);}catch (Exception e){}
+        hello.start();
+    }
+
+}
+
+class Hi extends Thread{
+
+    @Override
+    public void run () {
+        for (int i=0;i<5; i++){
+            System.out.println("Hi");
+            try{Thread.sleep(1000);}catch (Exception e){}
+        }
+    }
+}
+
+class Hello extends Thread{
+
+    @Override
+    public void run (){
+        for (int i=0;i<5; i++){
+            System.out.println("Hello");
+            try{Thread.sleep(1000);}catch (Exception e){}
+        }
+    }
 }
