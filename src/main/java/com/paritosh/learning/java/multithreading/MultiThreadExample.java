00eb7121e5454ea168b268e6150e5e7dd99f3e67
rparitosh <rparitosh@gmail.com>
2022-06-17 14:24:04 -0500

    join demo

--- src/main/java/com/paritosh/learning/java/multithreading/MultiThreadExample.java
+++ src/main/java/com/paritosh/learning/java/multithreading/MultiThreadExample.java
@@ -2,7 +2,7 @@ package com.paritosh.learning.java.multithreading;
 
 
 public class MultiThreadExample {
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
 
         Thread t1 = new Thread(() -> {
             for (int i=0;i<5; i++){
@@ -20,6 +20,15 @@ public class MultiThreadExample {
         t1.start();
         try{Thread.sleep(100);}catch (Exception e){}
         t2.start();
+        System.out.println("T1 alive before join : " + t1.isAlive());
+        System.out.println("T2 alive before join : " + t2.isAlive());
+
+        t1.join();
+        t2.join();
+        System.out.println("T1 alive after join : " + t1.isAlive());
+        System.out.println("T2 alive after join : " + t2.isAlive());
+
+        System.out.println("Bye");
     }
 
 }
